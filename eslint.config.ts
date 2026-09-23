2c3434a9199baa44da19bf62881936741da52be7
Rian <rianoleary@gmail.com>
2026-09-23 10:45:02 +0100

    Fix: correctly commiting the eslint.config file. Did not commit it last time

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,26 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro"
 
-export default [...eslintPluginAstro.configs.recommended];
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+
+  {
+  "settings": {
+    "import/core-modules": ["astro:content", "astro:transitions"],
+    "import/parsers": {
+      "astro-eslint-parser": [".astro"],
+      "espree": [".js", ".mjs", ".cjs"],
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    }
+  }
+}
+]
+
+
